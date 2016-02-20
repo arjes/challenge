5124dd8428fbd3e7ec5eb42ddda96d291cf5aedc
Brian Malinconico <brian.malinconico@damballa.com>
2016-02-20 14:39:11 -0500

    No mass-assignment of grade, that would be cheating

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -69,6 +69,6 @@ class PlayersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def player_params
-      params.require(:player).permit(:name, :email, :hand, :grade)
+      params.require(:player).permit(:name, :email, :hand)
     end
 end
